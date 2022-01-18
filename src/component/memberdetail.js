139f7ac5b7124e0675701202e3d8260751760389
CPX <chinnathorn.p@gmail.com>
2022-01-18 13:20:13 +0700

    firework effect

--- src/component/memberdetail.js
+++ src/component/memberdetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Card, Fade, Grow, CardMedia, Typography, Zoom, Link, Breadcrumbs } from '@material-ui/core';
+import { Card, Fade, Grow, CardMedia, Typography, Zoom, Link, Breadcrumbs, Button } from '@material-ui/core';
 
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import CakeIcon from '@material-ui/icons/Cake';
@@ -12,6 +12,23 @@ import PanToolIcon from '@material-ui/icons/PanTool';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import NaturePeopleIcon from '@material-ui/icons/NaturePeople';
 
+import { Fireworks } from 'fireworks-js/dist/react'
+
+var pm = new Audio('https://p.scdn.co/mp3-preview/26031551568cba193fbb55d6e4dcf3eb8fb99b04?cid=774b29d4f13844c495f206cafdad9c86')
+
+const fwoptions = {
+    speed: 8,
+    explosion: 6
+  }
+
+  const fwstyle = {
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    background: 'transperent'
+  }
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -22,6 +39,8 @@ function capitalizeFirstLetter(string) {
         const [arr, setArr] = React.useState([]); 
         const [Loaded, setLoaded] = React.useState(false);
         const [birthday, setBirthday] = React.useState(false);
+        
+        const [play, onPlay] = React.useState(false);
 
         const BirthdayCheck = (val) => {
             fetch(fet + '/bnk48/getmemberbybirth?tstamp=' + Math.floor( new Date().getTime()  / 1000), {
@@ -42,9 +61,15 @@ function capitalizeFirstLetter(string) {
         }
 
         const PlaySong = () => {
-            const pm = new Audio('https://p.scdn.co/mp3-preview/26031551568cba193fbb55d6e4dcf3eb8fb99b04?cid=774b29d4f13844c495f206cafdad9c86')
-            if (pm.duration > 0 && !pm.paused) { } else {
+            if (pm.paused) { 
                 pm.play()
+                onPlay(true)
+                var loop = setInterval(function () {
+                    if (pm.paused) { 
+                        clearInterval(loop)
+                        onPlay(false)
+                    }
+                }, 100);
             }
         }
 
@@ -78,6 +103,9 @@ function capitalizeFirstLetter(string) {
             } else {
                 History.push("/")
             }
+            return (() => {
+                pm.pause()
+            })
         }, [])
         return (  
         <>
@@ -94,7 +122,12 @@ function capitalizeFirstLetter(string) {
                     <>
                     {arr.length > 0 && arr.map((item, i) => (
                         <Grow in={Loaded} timeout={600}>
-                         <Card className={(window.innerWidth > 600 ? ' m-5' : ' m-3') + " row"} key={i}>
+                            <div>
+                            <Fade in={play} timeout={{enter:100,exit:500}}>
+                            <Fireworks options={fwoptions} style={fwstyle} />
+                            </Fade>
+
+                            <Card className={(window.innerWidth > 600 ? ' m-5' : ' m-3') + " row"} key={i}>
                             <div className={window.innerWidth > 1600 ? 'col-lg-3 mb-1' : 'col-lg-4 mb-1'}>
                                 <Fade in={true} timeout={600} style={{ transitionDelay: 300}}>
                                     <CardMedia
@@ -110,7 +143,7 @@ function capitalizeFirstLetter(string) {
                                     <>
                                         <h6><LocationOnIcon fontSize="small"/> {item.province}</h6>
                                         {birthday ? (
-                                            <h6 onClick={()=> PlaySong()}><CakeIcon fontSize="small"/> Today is her birthday! ({new Date().getFullYear() - new Date(item.birthday).getFullYear() + ' years old'})</h6>
+                                            <h6><CakeIcon fontSize="small"/> Today is her birthday! ({new Date().getFullYear() - new Date(item.birthday).getFullYear() + ' years old'})</h6>
                                         ) : (
                                             <h6><CakeIcon fontSize="small"/> {item.birthday}</h6>
                                         )}
@@ -143,9 +176,14 @@ function capitalizeFirstLetter(string) {
                                     </>
                                     <hr />
                                     <a className='text-dark' href={item.ref} target='_blank'>Reference from BNK48 official Site</a>
+                                    <br />
+                                    {birthday && (
+                                        <Button onClick={()=> PlaySong()} className='mt-3' color="primary" variant="contained">Click here see effect</Button>  
+                                    )}
                                 </div>
                         </Fade>
                     </Card>
+                            </div>
                     </Grow>
                     ))}
                     </>

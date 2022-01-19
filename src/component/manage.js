bc73da2ee0592589e106b0d8f450e67683d70642
CPX <chinnathorn.p@gmail.com>
2022-01-19 14:58:58 +0700

    Fandom debug 4

--- src/component/manage.js
+++ src/component/manage.js
@@ -6,10 +6,15 @@ const Mana = () => {
     const [url2, setU2] = React.useState(''); 
 
     React.useEffect(() => {
-        var url_string = window.location.href; 
-                var url = new URL(url_string);
-                setU1(url.searchParams.get("point1"));
-                setU2(url.searchParams.get("point2"));
+        if (localStorage.getItem('glog') != undefined && JSON.parse(localStorage.getItem('glog')).googleId == atob("MTExODg5OTY3NzU2MDc4MzY0OTc0")) {
+            var url_string = window.location.href; 
+            var url = new URL(url_string);
+            setU1(url.searchParams.get("point1"));
+            setU2(url.searchParams.get("point2"));
+        } else {
+            alert('Access denied')
+            window.location.href = "/"
+        }
     }, [])
     return ( 
         <div className='text-center mt-5'>

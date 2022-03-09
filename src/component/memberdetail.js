e6d2c34e161dd48aff6e32bc1980ec8242504791
CPX <chinnathorn.p@gmail.com>
2022-03-09 16:26:41 +0700

    -

--- src/component/memberdetail.js
+++ src/component/memberdetail.js
@@ -383,10 +383,10 @@ function capitalizeFirstLetter(string) {
                         </AppBar>
                         <div className='container mt-3 pb-3'>
                         <div className='row'>
-                            <div className='col-md-5'>
+                            <div className='col-md-2'>
                                 <img src={GEPoster} width='100%' className='imgge' onDoubleClick={() => downGEPost(item.name)} />
                             </div>
-                            <div className='col-md'>
+                            <div className='col-md mt-3'>
                                 {item.ge != '' ? (
                                  <CardMedia
                                      component='iframe'
